d7b143784c1fbaf735a94a08cdaaf71f98f17885
Harshit chemical industry <pressmediacompany@gmail.com>
2026-07-28 22:49:17 +0530

    Update products.js

--- src/data/products.js
+++ src/data/products.js
@@ -1,1 +1,10 @@
+const BASE_IMAGE_URL = "https://githubusercontent.com";
+
+export const products = [
+  { id: 1, name: "21 Inch Floor Wiper", price: 240, category: "Wiper", image: `${BASE_IMAGE_URL}21-inch-floor-wiper.jpg` },
+  { id: 2, name: "Multipurpose Plastic Storage Bucket Drum", price: 450, category: "Bucker & Tub", image: `${BASE_IMAGE_URL}Multipurpose_plastic_storage_bucket_drum.jpg` },
+  { id: 3, name: "Bathroom Cleaner", price: 120, category: "Cleaners", image: `${BASE_IMAGE_URL}bathroom_cleaner.jpg` },
+  // ... (baaki 67 products yahan pe aapke diye gaye code se aayenge)
+  { id: 70, name: "Window Wiper", price: 130, category: "Wiper", image: `${BASE_IMAGE_URL}window_wiper.jpg` }
+];
 
