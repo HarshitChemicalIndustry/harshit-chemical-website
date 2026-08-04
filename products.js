1be2dd3a97ea20d4c6dfef678d9c9f0b33520e89
Harshit chemical industry <pressmediacompany@gmail.com>
2026-08-04 19:52:55 +0530

    Update products.js

--- products.js
+++ products.js
@@ -1,1 +1,42 @@
-
+const products = [
+    {
+        name: "Room Spray",
+        image: "images/room_spray.jpg",
+        price: "₹120"
+    },
+    {
+        name: "Car Wash Shampoo",
+        image: "images/car_bike_shampoo.jpg",
+        price: "₹250"
+    },
+    {
+        name: "Carpet Shampoo",
+        image: "images/carpet_shampoo.jpg",
+        price: "₹100"
+    },
+    {
+        name: "Hard Spot Cleaner",
+        image: "images/hardspot_cleaner.jpg",
+        price: "₹80"
+    },
+    {
+        name: "Phenyl",
+        image: "images/phenyl.jpg",
+        price: "₹100"
+    },
+    {
+        name: "Toilet Cleaner",
+        image: "images/toilet_cleaner.jpg",
+        price: "₹120"
+    },
+    {
+        name: "Hand Wash",
+        image: "images/hand_wash.jpg",
+        price: "₹150"
+    },
+    {
+        name: "Dish Wash",
+        image: "images/dish_wash.jpg",
+        price: "₹100"
+    }
+];
