48f1ac21288449c2ab46d01cfddb59867067492f
Harshit chemical industry <pressmediacompany@gmail.com>
2026-08-04 19:57:36 +0530

    Implement cart functionality with add and show methods

--- cart.js
+++ cart.js
@@ -1,1 +1,23 @@
+let cart = [];
 
+function addToCart(productName, price) {
+    cart.push({
+        name: productName,
+        price: price
+    });
+
+    alert(productName + " cart में जोड़ दिया गया");
+    localStorage.setItem("cart", JSON.stringify(cart));
+}
+
+function showCart() {
+    let savedCart = JSON.parse(localStorage.getItem("cart")) || [];
+
+    let message = "आपका Cart:\n\n";
+
+    savedCart.forEach(item => {
+        message += item.name + " - " + item.price + "\n";
+    });
+
+    alert(message);
+}
