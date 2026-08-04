a38624a713483e52cb343cd98f6c384b78a37c2c
Harshit chemical industry <pressmediacompany@gmail.com>
2026-08-04 19:32:19 +0530

    Add search feature to filter product cards
    Implement search functionality for product cards

--- js/app.js
+++ js/app.js
@@ -1,1 +1,25 @@
+// ===== Harshit Mart App =====
 
+let searchBox = document.getElementById("searchBox");
+
+if(searchBox){
+searchBox.addEventListener("keyup",function(){
+
+let value=this.value.toLowerCase();
+
+let cards=document.querySelectorAll(".product-card");
+
+cards.forEach(function(card){
+
+let name=card.querySelector("h3").innerText.toLowerCase();
+
+if(name.indexOf(value)>-1){
+card.style.display="flex";
+}else{
+card.style.display="none";
+}
+
+});
+
+});
+}
